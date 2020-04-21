9a6109fc931b805f6db0a50ff287c597aac614de
masterkn48 <masterkn48@gmail.com>
2020-04-22 02:16:20 +0530

    validation

---
+++ Readme.md
@@ -0,0 +1,21 @@
+## MasterCoin: A Complete CryptoCurrency Blockchain
+
+<hr/>
+
+##### My first Blockchain project in learning path of blockchain.
+
+- Using SHA256 algorthims for hashing
+
+- Consensus Algorithm: Proof of Work
+
+- Client GUI : React.js*
+
+- Backend : Node.js
+
+- CryptoCurrency will be MasterCoin
+
+- This blockchain will be fallback  to untampered blockchain if sometime try to tampered
+
+  > working live link will be updated when GUI  done
+
+  
--- main.js
+++ main.js
@@ -1,10 +1,63 @@
-class block {
+const SHA256 = require("crypto-js/sha256");
+class Block {
   constructor(index, timestamp, data, previousHash = "") {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = "";
+    this.hash = this.calculateHash();
   }
-  calculateHash() {}
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data).toString()
+    );
+  }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+  createGenesisBlock() {
+    return new Block(0, "01/01/20", "Genesis block", "0");
+  }
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    // in future consensus algorthims will be added
+    this.chain.push(newBlock);
+  }
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        //console.log("object1");
+        return false;
+      }
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        //console.log("object");
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+let masterCoin = new Blockchain();
+masterCoin.addBlock(new Block(1, "10/01/2020", { amount: 4 }));
+masterCoin.addBlock(new Block(2, "10/02/2020", { amount: 12 }));
+
+//console.log(JSON.stringify(masterCoin, null, 4));
+console.log("Is Blockchain valid? " + masterCoin.isChainValid());
+
+masterCoin.chain[1].data = { amount: 100 };
+masterCoin.chain[1].hash = masterCoin.chain[1].calculateHash();
+
+console.log("Is Blockchain valid? " + masterCoin.isChainValid());
