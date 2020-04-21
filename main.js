549cda2e0e987f11219010249122d5948ab6e5b4
masterkn48 <masterkn48@gmail.com>
2020-04-22 01:23:14 +0530

    initial blockchain boilerplate

---
+++ main.js
@@ -0,0 +1,10 @@
+class block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = "";
+  }
+  calculateHash() {}
+}
